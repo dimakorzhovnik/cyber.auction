7206c34f659af77363d18e99a4dae1d61a7c9a83
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-09-02 11:51:18 +0300

    init gh-pages

--- src/components/ui/Dots.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Dots = ({ big }) => (
-  <div className={big ? 'loader schedule' : 'loader'}>
-    <span>.</span>
-    <span>.</span>
-    <span>.</span>
-  </div>
-);
